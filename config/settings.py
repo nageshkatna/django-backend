8507435e46f8ffe11ac7e317f511aeb0f7e92677
nageshkatna <nagesh.katna@gmail.com>
2025-08-07 16:49:48 -0400

    fixes

--- config/settings.py
+++ config/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = 'django-insecure-%4z2_30m_h6h#9ws8*-!g$lntub&cap#%brv2g$7!$uo3*r+#i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
